f4e157aac9cab91e52cd7a33ac9c3017620dd393
Fine <Fine@DESKTOP-1QB7RCK>
2018-08-27 14:15:45 +0900

    Thymeleaf 타임리프

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -2,6 +2,8 @@ package org.zerock.controller;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -76,6 +78,20 @@ public class SampleController {
 		
 		String result= "SUCCESS!";
 		model.addAttribute("result", result);
+	}
+	
+	@GetMapping("/sample7")
+	public void sample7(Model model) {
+		
+		model.addAttribute("now", new Date());
+		model.addAttribute("price", 123456789);
+		model.addAttribute("title", "This is a just sample.");
+		model.addAttribute("options", Arrays.asList("AAAA","BBB","CCC","DDD"));
+	}
+	
+	@GetMapping("/sample8")
+	public void sample8(Model model) {
+		
 	}
 
 }
